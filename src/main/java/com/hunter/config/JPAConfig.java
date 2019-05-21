0640a7e0d7268613e9964842930f176c1750dfb9
李淙浩 <15018357143@163.com>
2019-05-21 08:01:45 +0800

    Initial commit

---
+++ src/main/java/com/hunter/config/JPAConfig.java
@@ -0,0 +1,43 @@
+package com.hunter.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.hunter.repository")
+@EnableTransactionManagement
+public class JPAConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource(){
+        return DataSourceBuilder.create().build();
+    }
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(){
+        HibernateJpaVendorAdapter jpaVendor = new HibernateJpaVendorAdapter();
+        jpaVendor.setGenerateDdl(false);
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setJpaVendorAdapter(jpaVendor);
+        entityManagerFactoryBean.setPackagesToScan("com.hunter.entity");
+        return entityManagerFactoryBean;
+    }
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+}
